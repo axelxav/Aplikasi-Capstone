44e7323a0a7eeb4293c0c41d20cfdb2b508b02d1
axelxav <axel.napitupulu@gmail.com>
2024-11-06 04:22:53 +0700

    feat: adding arrival status fetching store

--- store/reservationStore.ts
+++ store/reservationStore.ts
@@ -3,11 +3,15 @@ import { create } from "zustand";
 interface ReservationStore {
   reservation_qr: string;
   setReservationQR: (qr: string) => void;
+  startCount: boolean;
+  setStartCount: (start: boolean) => void;
 }
 
 const useReservationStore = create<ReservationStore>((set) => ({
   reservation_qr: '',
-  setReservationQR: (qr) => set({ reservation_qr: qr })
+  setReservationQR: (qr) => set({ reservation_qr: qr }),
+  startCount: false,
+  setStartCount: (start) => set({ startCount: start }),
 }));
 
 export default useReservationStore;
